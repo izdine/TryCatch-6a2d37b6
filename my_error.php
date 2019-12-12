fd602b2f930f5d6a17996bddd381e3713000d634
Iz-Dine Amadou <2088346@talnet.nl>
2019-12-12 13:58:48 +0100

    Uitwerking van de exercise

---
+++ my_error.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+  <form  method="post">
+    <input type="text" id="login"placeholder="login" name=login>
+    <input type="submit" name="button" value=""></input>
+  </form>
+<?php
+function countDown()
+{
+if ($_POST["login"] > 10 && $_POST < 1) {
+  throw new Exception('ERROR');
+}
+return
+}
+try {
+    echo inverse(10) . "\n";
+    echo inverse(0) . "\n";
+} catch (Exception $a) {
+    echo 'Er is iets mis gegaan: ', $a->getMessage(), "\n";
+}
+echo "Er is iets mis gegaan";
+
+ ?>
+
+  </body>
+</html>
